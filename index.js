e3c447aedd4ce836186b4470f31b4cfc4df651cd
DoctaDocta <trashm96@gmail.com>
2015-07-25 13:19:02 -0400

    added songs folder

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.set('port', 8080);
 app.set('views', __dirname + '/public');
 app.set('view engine', 'html');
 app.engine('html', engine.mustache);
-
 app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) 
 {
 	res.render('index.html');
@@ -18,3 +16,4 @@ app.get('/', function(req, res)
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port' + app.get('port'));
 });
+
