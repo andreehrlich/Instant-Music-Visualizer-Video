8323a2f30fc90c8542c4a03c5fbccf1e53d100c6
DoctaDocta <trashm96@gmail.com>
2015-07-25 11:51:38 -0400

    made server and index.html to display locally

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var engine = require('consolidate');
+
+var app = express();
+
+app.set('port', 8080);
+app.set('views', __dirname + '/public');
+app.set('view engine', 'html');
+app.engine('html', engine.mustache);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) 
+{
+	res.render('index.html');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express server listening on port' + app.get('port'));
+});
