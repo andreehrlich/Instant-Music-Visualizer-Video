4357ebd577b473456d05ee1ab776efc5d8026b93
Joseph Lap <Joseph@shelley-saltzmans-macbook-pro-4.local>
2015-07-25 18:18:47 -0400

    visualized said visualizer

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,14 @@
+var capture;
 
 function setup() {
-
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter('INVERT');
 }
