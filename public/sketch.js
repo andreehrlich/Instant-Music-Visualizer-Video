83349580772f1fc7a0409469ec59709a99bcf803
DoctaDocta <trashm96@gmail.com>
2015-07-25 18:32:08 -0400

    added visual and song to sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,29 @@
 var capture;
+var song;
+
+function preload() {
+  song = loadSound('songs/missingyou.mp3');
+}
 
 function setup() {
-  createCanvas(390, 240);
+  createCanvas(1600, 900);
   capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
+  capture.size(1600, 900);
+  capture.hide();
+  song.play();
 }
 
 function draw() {
   background(255);
-  image(capture, 0, 0, 320, 240);
+  image(capture, 0, 0, 1600, 900);
   filter('INVERT');
+}
+
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.pause(); // .play() will resume from .pause() position
+  } else {
+    song.play();
+  }
 }
