15d03cdd92d5a5a30eded4bde8a9b6bfbb9cbf87
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2015-07-25 15:37:41 -0400

    Sending data to server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var engine = require('consolidate');
+var sys = require('sys');
+var exec = require('child_process').exec;
 
+var child;
 var app = express();
 
 app.set('port', 8080);
@@ -12,6 +15,21 @@ app.get('/', function(req, res)
 {
 	res.render('index.html');
 });
+
+app.get('/download', function(req, res) 
+{	
+	//var songURL = req.body.url;
+	var songURL = "https://soundcloud.com/tink_g/tink-i-like-prod-c-sick";
+	console.log(req);
+	console.log(songURL);
+	var cmd = 'cd public/songs && youtube-dl ' + songURL;
+	exec(cmd, function(error, stdout, stderr) {
+	  // command output is in stdout
+	});
+	res.send("Yay!");
+});
+
+
 
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port' + app.get('port'));
