571a243ef14f2f1aca18103bfc6a3e0029d47f95
DoctaDocta <trashm96@gmail.com>
2015-08-13 02:21:08 -0400

    bootstrapped css. broken visualizer.

--- public/sketch.js
+++ public/sketch.js
@@ -5,19 +5,19 @@ var width = 800;
 var myDiv;
 
 function preload() {
-  song = loadSound('songs/aaliyah.mp3');
+  song = loadSound('songs/missingyou.mp3');
 }
 
 function setup() {
-  createCanvas(1600, 900);
+  createCanvas(800, 500);
   capture = createCapture(VIDEO);
-  capture.size(1600, 900);
+  capture.size((800, 500);
   capture.hide();
   song.play();
   amplitude = new p5.Amplitude();
   myDiv = createDiv();
-  myDiv.size([1600], [900]);
- 
+  myDiv.size([800], [500]);
+
   myDiv.style("top", "0");
   myDiv.style("position", "fixed");
   myDiv.style("z-index", "1000");
@@ -27,17 +27,16 @@ function setup() {
 }
 
 function draw() {
-	var level = amplitude.getLevel();
-	//console.log(level)
-  	background(255);
-  	var red = ceil(.5*(100 + 1000*level));
-  	var blue = ceil(.5*(10 * (1/level)));
-  	var green = ceil(.5*(100 / level));
-
-  	var rgbString = "rgb("+red+", " + green + ", " + blue+")";
-  	console.log(rgbString);
- 	myDiv.style("background-color", rgbString);
-  	image(capture, 0, 0, 1600, 900);
+  var level = amplitude.getLevel();
+  //console.log(level)
+  background(255);
+  var red = ceil(.5*(100 + 1000*level));
+  var blue = ceil(.5*(10 * (1/level)));
+  var green = ceil(.5*(100 / level));
+  var rgbString = "rgb("+red+", " + green + ", " + blue+")";
+  console.log(rgbString);
+  myDiv.style("background-color", rgbString);
+  image(capture, 0, 0, 800, 500);
   //filter('INVERT');
 }
 
